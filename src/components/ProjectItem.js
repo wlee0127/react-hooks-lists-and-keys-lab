ccfe9da7d7f407eb98c06c8b3d1a110b7f63defa
william lee <will.lee.npc@gmail.com>
2023-12-18 23:00:55 -0500

    complete lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,20 @@
 import React from "react";
 
+
+
 function ProjectItem({ name, about, technologies }) {
+  const  techElements = technologies.map((tech)=>{
+    return <span key={tech}>{tech}</span>
+  });
+  console.log(techElements)
+
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {techElements}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,23 @@
 import React from "react";
-import ProjectItem from "./ProjectItem";
+
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const projectElements = projects.map((project)=>{
+    return (
+      <div key={project.id}>
+      <h3>{project.name}</h3>
+      <h3>{project.about}</h3>
+      {project.technologies.join(", ")}
+      </div>
+    )
+  })
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectElements}</div>
     </div>
   );
 }
